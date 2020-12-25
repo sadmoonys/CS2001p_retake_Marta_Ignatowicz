2949ab02d91d88343d5871fbae4457c865875c72
Mirna <73242980+mirnaalsaeed@users.noreply.github.com>
2020-12-25 15:58:07 +0000

    sign in

--- front/src/App.js
+++ front/src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import LandingIndex from './LandingPage/LandingIndex'
 
 
+
+
+
 function App() {
   return (
     <LandingIndex/> 
+
      
   );
 }
 
 export default App;
+
---
+++ front/src/LandingPage/signin.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function signin() {
+
+return (
+
+<div>
+<h1> SIGN IN  Page</h1>
+
+</div>
+
+);
+}
+
+export default signin;
