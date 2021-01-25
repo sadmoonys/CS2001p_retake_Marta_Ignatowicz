c0c1e3ee67d9faa8efb70cec5ca73bcf70b7cdcd
NikhilManikala <56125383+NikhilManikala@users.noreply.github.com>
2021-01-25 23:21:53 +0000

    Updated Project

---
+++ Back-End/noteware/src/main/java/com/group15/noteware/NotewareApplication.java
@@ -0,0 +1,13 @@
+package com.group15.noteware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotewareApplication.class, args);
+	}
+
+}
---
+++ Back-End/noteware/src/test/java/com/group15/noteware/NotewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group15.noteware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
