2715626944b7bcfb5b53f0718531c78ea571e085
NikhilManikala <56125383+NikhilManikala@users.noreply.github.com>
2021-01-26 09:48:04 +0000

    Fixed Errors

--- Back-End/noteware/src/main/java/com/group15/noteware/security/WebSecurityConfig.java
+++ Back-End/noteware/src/main/java/com/group15/noteware/security/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.group15.noteware.security;
 
+import com.group15.noteware.security.jwt.AuthTokenFilter;
+import com.group15.noteware.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
