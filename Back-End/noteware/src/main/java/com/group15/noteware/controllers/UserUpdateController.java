63f5d63f56522d7695a6a1194628e261e8e18d32
sadmoonys <73638941+sadmoonys@users.noreply.github.com>
2021-02-03 17:57:03 +0000

    Profiles API completed.

--- Back-End/noteware/src/main/java/com/group15/noteware/controllers/UserUpdateController.java
+++ Back-End/noteware/src/main/java/com/group15/noteware/controllers/UserUpdateController.java
@@ -1,13 +1,15 @@
 package com.group15.noteware.controllers;
 
 import com.group15.noteware.models.User;
+import com.group15.noteware.payload.request.UserUpdateRequest;
+import com.group15.noteware.payload.response.MessageResponse;
 import com.group15.noteware.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -17,6 +19,17 @@ public class UserUpdateController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @PostMapping("/updateprofile")
+    public ResponseEntity<?> updateUser(@Valid @RequestBody UserUpdateRequest userUpdateRequest){
+        userRepository.findByUsername(userUpdateRequest.getUsername()).map(target ->{
+            target.setBio(userUpdateRequest.getBio());
+            target.setCurrentCourse(userUpdateRequest.getCurrentCourse());
+            return target;
+        });
+
+        return ResponseEntity.ok(new MessageResponse("User updated successfully"));
+    }
 
     @Transactional
     public Optional<User> update(String username, User user){
