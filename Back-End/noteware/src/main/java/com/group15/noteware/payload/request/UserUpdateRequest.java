7829155533d2f06b33d08eb031c0273ad9155356
sadmoonys <73638941+sadmoonys@users.noreply.github.com>
2021-02-03 17:39:27 +0000

    Created a class to process user update request.

---
+++ Back-End/noteware/src/main/java/com/group15/noteware/payload/request/UserUpdateRequest.java
@@ -0,0 +1,28 @@
+package com.group15.noteware.payload.request;
+
+import javax.validation.constraints.Size;
+
+public class UserUpdateRequest {
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getCurrentCourse() {
+        return currentCourse;
+    }
+
+    public void setCurrentCourse(String currentCourse) {
+        this.currentCourse = currentCourse;
+    }
+
+    @Size (min = 0, max = 300)
+    private String bio;
+
+    @Size(min = 0, max = 50)
+    private String currentCourse;
+}
