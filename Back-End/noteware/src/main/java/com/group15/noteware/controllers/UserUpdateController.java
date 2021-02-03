5aa9246ea5265b81dfa6d0b1bbb0ee6addd7eb06
NikhilManikala <56125383+NikhilManikala@users.noreply.github.com>
2021-02-03 18:02:50 +0000

    Removed unnecessary imports

--- Back-End/noteware/src/main/java/com/group15/noteware/controllers/UserUpdateController.java
+++ Back-End/noteware/src/main/java/com/group15/noteware/controllers/UserUpdateController.java
@@ -1,16 +1,13 @@
 package com.group15.noteware.controllers;
 
-import com.group15.noteware.models.User;
 import com.group15.noteware.payload.request.UserUpdateRequest;
 import com.group15.noteware.payload.response.MessageResponse;
 import com.group15.noteware.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
