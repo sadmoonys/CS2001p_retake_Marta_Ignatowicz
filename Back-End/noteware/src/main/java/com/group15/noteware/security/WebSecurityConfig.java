22ce2653b286744dbf1f89a7552699f5e3d427e4
sah-sen <73240215+sah-sen@users.noreply.github.com>
2021-02-15 14:37:11 +0000

    added Autowire

--- Back-End/noteware/src/main/java/com/group15/noteware/security/WebSecurityConfig.java
+++ Back-End/noteware/src/main/java/com/group15/noteware/security/WebSecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(
@@ -53,51 +54,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         return new BCryptPasswordEncoder();
     }
-  //Here is a bean i made
-/*  @Bean
-  public CustomOAuth2UserService oAuth2UserService(){
-      return oAuth2UserService;
-  }*/
-    //delete this if it causes problems
+
+
+    //I put the autowire here, maybe it could help
+    @Autowired
+    private CustomOAuth2UserService oAuth2UserService;
+    //Everything started breaking after i put this in. but it doesn't seem to be throwing any errors
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-  /*      http.authorizeRequests()
-                //v  for this, check the .html class for the google oauth 2 button url
-                .antMatchers("/oauth2/**").permitAll()
-                .antMatchers(("/user/**")).authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .usernameParameter("email")
-                    .permitAll()
-                    .defaultSuccessUrl("/")
-                .and()
-                .oauth2Login()
-                    .loginPage("/login")
-                    .userInfoEndpoint().userService(oAuth2UserService)
-                    .and()
-                .and()
-                .logout().permitAll()
-                .and()
-      //          .rememberMe().tokenRepository();  //there's supposed to be a persistentTokenRepository() here
-;
-*/
-
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                    .antMatchers("/oauth2/**").permitAll()
+                //^look at the URL on the google button, it should look like oauth2/authorization/google
                 .antMatchers("/api/test/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
-/*
-    @Autowired
-    private CustomOAuth2UserService oAuth2UserService;
-    //the CustomOAuth2UserService bean was made because line 63-64 was throwing an error that there was no beans
-*/
+
+
+
 }
