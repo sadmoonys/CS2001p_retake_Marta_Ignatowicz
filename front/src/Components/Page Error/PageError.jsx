a7dc4e43cbd9bc3459a7b5a3cc64b39fac470da8
WajaTheHat <73642192+WajaTheHat@users.noreply.github.com>
2021-02-24 00:29:41 +0000

    404 Page
    Added description of page

---
+++ front/src/Components/Page Error/PageError.jsx
@@ -0,0 +1,4 @@
+<p className="Sorry">Something went wrong!</p>
+
+<p className="Exist">The page you are looking for doesn’t exist or an other error has occurred.</p><
+Go back, or head or head over to noteware.com to choose a new direction.
