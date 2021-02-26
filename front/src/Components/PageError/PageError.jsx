803727d3600b61d1b72f1e9ac5d20ed801513d46
WajaTheHat <73642192+WajaTheHat@users.noreply.github.com>
2021-02-26 20:08:02 +0000

    CSS update - 404 page

--- front/src/Components/PageError/PageError.jsx
+++ front/src/Components/PageError/PageError.jsx
@@ -6,11 +6,14 @@ class PageError extends Component {
     render() { 
         return (  
            <html>
+           
+            <div class="container-404">
             <p className="Sorry">Something went wrong!</p>
-            <img src={Error} alt=""/>
+            <img class="image-404" src={Error} alt=""/>
             <p className='Four'> 404</p>
             <p className="Exist">The page you are looking for doesn’t exist or an other error has occurred.Go back,
              or head or head over to noteware.com to choose a new direction.</p>
+        </div>
             
 
         </html> 
