af3cda5b454a02a2ab1191d558fc79b00dd4b33e
sah-sen <73240215+sah-sen@users.noreply.github.com>
2021-02-28 20:06:18 +0000

    update Note

--- Back-End/noteware/src/main/java/com/group15/noteware/models/Note.java
+++ Back-End/noteware/src/main/java/com/group15/noteware/models/Note.java
@@ -58,5 +58,11 @@ public class Note {
                 ", text='" + text + '\'' +
                 '}';
     }
+
+    @ManyToMany(fetch =FetchType.LAZY)
+    @JoinTable(name = "note_category",
+            joinColumns = @JoinColumn(name = "note_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categorySet = new HashSet<>();
 }
 
