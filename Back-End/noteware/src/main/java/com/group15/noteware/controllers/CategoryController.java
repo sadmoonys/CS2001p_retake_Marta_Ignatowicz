f3a1ddaaf8f0ea9387a42b0e7aa925ee13071c63
sah-sen <73240215+sah-sen@users.noreply.github.com>
2021-03-01 22:07:57 +0000

    Update CategoryController.java

--- Back-End/noteware/src/main/java/com/group15/noteware/controllers/CategoryController.java
+++ Back-End/noteware/src/main/java/com/group15/noteware/controllers/CategoryController.java
@@ -1,4 +1,26 @@
 package com.group15.noteware.controllers;
 
+
+import com.group15.noteware.models.Category;
+import com.group15.noteware.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public class CategoryController {
+    @Autowired
+    CategoryRepository CategoryRepository;
+
+    @GetMapping(value = "/load")
+    public List<Category> getAll() {
+        return CategoryRepository.findAll();
+    }
+
+
+
+
 }
+
+
