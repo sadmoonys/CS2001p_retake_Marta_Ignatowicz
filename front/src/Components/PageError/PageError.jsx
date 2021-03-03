f348f98dbe8170a59352b30ac07b371c482f6bd8
WajaTheHat <73642192+WajaTheHat@users.noreply.github.com>
2021-03-03 23:35:05 +0000

    Error PAGE COMPLETED
    Fin

--- front/src/Components/PageError/PageError.jsx
+++ front/src/Components/PageError/PageError.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from 'react'
 import './PageError.css'
-import Error  from '../../Images/sadfaceforerror.png'
+import Error from '../../Images/sadfaceforerror.png'
 
 class PageError extends Component {
-    render() { 
-        return (  
-           <html>
-           
-            <div class="container-404">
-            <p className="Sorry">Something went wrong!</p>
-            <img class="image-404" src={Error} alt=""/>
-            <p className='Four'> 404</p>
-            <p className="Exist">The page you are looking for doesn’t exist or an other error has occurred.
-            Go back, or head or head over to noteware.com to choose a new direction.</p>
-        </div>
+    render() {
+        return (
             
 
-        </html> 
+                <div class="container-404">
+                    <p className="Sorry">Something went wrong!</p>
+                    <img class="image-404" src={Error} alt="" />
+                    <p className='Four'> 404</p>
+                    <p className="Exist">The page you are looking for doesn’t exist or an other error has occurred.
+            Go back, or head or head over to noteware.com to choose a new direction.</p>
+                </div>
+
+
+        
         );
     }
 }
