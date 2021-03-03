3f451048211b514b43e2a8632be7e09591f4c56a
sah-sen <73240215+sah-sen@users.noreply.github.com>
2021-03-03 13:17:05 +0000

    Update CategoryController.java

--- Back-End/noteware/src/main/java/com/group15/noteware/controllers/CategoryController.java
+++ Back-End/noteware/src/main/java/com/group15/noteware/controllers/CategoryController.java
@@ -3,12 +3,16 @@ package com.group15.noteware.controllers;
 
 import com.group15.noteware.models.Category;
 import com.group15.noteware.models.Note;
+import com.group15.noteware.payload.request.notesUpdateRequest;
+import com.group15.noteware.payload.response.MessageResponse;
 import com.group15.noteware.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.Valid;
 import java.util.List;
 
 public class CategoryController {
@@ -26,8 +30,17 @@ public class CategoryController {
         CategoryRepository.save(category);
         return CategoryRepository.findAll();
     }
-    
 
+   // @PostMapping("/updateCategory")
+    //fic the "notesUpdateRequest" to change to "categoryUpdateRequest"
+  //  public ResponseEntity<?> updateCategory(@Valid @RequestBody note UpdateRequest) {
+    //    CategoryRepository.findById(UpdateRequest.getId()).map(target -> {
+      //      target.setCategory(UpdateRequest.getCategory);
+      //      CategoryRepository.save(target);
+       //     return target;
+       // });
+     //   return ResponseEntity.ok(new MessageResponse("Category updated successfully"));
+  //  }
 
 
 }
