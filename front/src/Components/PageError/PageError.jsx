e31b0f15cc53164b010facd61da2784ca3569315
WajaTheHat <73642192+WajaTheHat@users.noreply.github.com>
2021-03-03 19:54:10 +0000

    404
    re positioned and made 404 bigger

--- front/src/Components/PageError/PageError.jsx
+++ front/src/Components/PageError/PageError.jsx
@@ -11,8 +11,8 @@ class PageError extends Component {
             <p className="Sorry">Something went wrong!</p>
             <img class="image-404" src={Error} alt=""/>
             <p className='Four'> 404</p>
-            <p className="Exist">The page you are looking for doesn’t exist or an other error has occurred.Go back,
-             or head or head over to noteware.com to choose a new direction.</p>
+            <p className="Exist">The page you are looking for doesn’t exist or an other error has occurred.
+            Go back, or head or head over to noteware.com to choose a new direction.</p>
         </div>
             
 
