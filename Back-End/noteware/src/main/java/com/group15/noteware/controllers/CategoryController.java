54a27b57290f3c15243c6fe0a9e109c91bdbe062
sah-sen <73240215+sah-sen@users.noreply.github.com>
2021-03-03 16:29:45 +0000

    Delete CRUD

--- Back-End/noteware/src/main/java/com/group15/noteware/controllers/CategoryController.java
+++ Back-End/noteware/src/main/java/com/group15/noteware/controllers/CategoryController.java
@@ -7,10 +7,9 @@ import com.group15.noteware.payload.request.notesUpdateRequest;
 import com.group15.noteware.payload.response.MessageResponse;
 import com.group15.noteware.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -26,23 +25,19 @@ public class CategoryController {
 
 
     @PostMapping(value = "/sendCategory")
-    public List<Category> persist(@RequestBody final Category category){
+    public List<Category> persist(@RequestBody final Category category) {
         CategoryRepository.save(category);
         return CategoryRepository.findAll();
     }
 
-   // @PostMapping("/updateCategory")
-    //fic the "notesUpdateRequest" to change to "categoryUpdateRequest"
-  //  public ResponseEntity<?> updateCategory(@Valid @RequestBody note UpdateRequest) {
-    //    CategoryRepository.findById(UpdateRequest.getId()).map(target -> {
-      //      target.setCategory(UpdateRequest.getCategory);
-      //      CategoryRepository.save(target);
-       //     return target;
-       // });
-     //   return ResponseEntity.ok(new MessageResponse("Category updated successfully"));
-  //  }
 
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(Category id) {
+        CategoryRepository.delete(id);
+    }
 
 }
+
 
 
