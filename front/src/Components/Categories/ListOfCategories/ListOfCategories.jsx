d36e2e26fee07c6fb5eb381b6c684e4fdf743722
sah-sen <73240215+sah-sen@users.noreply.github.com>
2021-03-06 16:23:41 +0000

    Create ListOfCategories.jsx

---
+++ front/src/Components/Categories/ListOfCategories/ListOfCategories.jsx
@@ -0,0 +1,30 @@
+  
+import React, { Component } from "react";
+//import NoteCard from "../NoteCard/NoteCard";
+//import "./ListOfNotesStyle.css"
+class ListOfCategories extends Component {
+ 
+  render() {
+    return (
+      <ul className="CategoryArea">
+        {this.props.notes.map((note, index) => {
+          return (
+            <div className="categoriesAlone" key={index}>
+
+              <NoteCard 
+              indice = {index}
+              deleteNote={this.props.deleteCategory}
+              updateNote={this.props.updateCategory}
+              title={note.title} 
+              text={note.text}
+              id={note.id}
+              />
+            </div>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default ListOfNotes;
