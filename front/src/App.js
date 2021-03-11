d64219fa8c26806f802c2e3bd6c49ff81a90d55c
surajlifeean <surajjeswara@yahoo.com>
2021-03-11 16:44:09 +0530

    Created note card component

--- front/src/App.js
+++ front/src/App.js
@@ -1,31 +1,7 @@
-import React from 'react';
-import LandingIndex from './Components/LandingPage/LandingIndex'
-import SignIn from './Components/SignInPage/SignInPage'
-import Signup from './Components/Signuppage/Signuppage'
-import CreateNote from './Components/CreateNote/CreateNote'
-import UserProfile from './Components/Userprofile/Userprofile'
-import UpdateProfile from './Components/UpdateProfile/UpdateProfile'
-import Categories from './Components/Categories/Categories'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import ProtectedRoute from './ProtectedRoute'
+import NotePage from './pages/notePage';
 
 function App() {
-    return ( 
-        <Router>
-            <Switch>
-                <Route exact path="/SignIn" component={SignIn}/>
-                <Route exact path="/Signup" component={Signup}/>
-                <Route exact path="/CreateNote" component={CreateNote}/>
-                <Route exact path="/Categories" component={Categories}/>
-                <Route exact path="/UserProfile" component={UserProfile}/>
-                <ProtectedRoute exact path="/UpdateProfile" component={UpdateProfile}/>
-                <Route exact path="/" component={LandingIndex}/>
-                <Route path="*" component={() => "404 NOT FOUND"}/>
-                
-            </Switch>
-        </Router>
-        
-    );
+	return <NotePage />;
 }
 
 export default App;
