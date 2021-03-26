d46b6afd6efd6519cfa1c5a5e88dad694804a453
leonardo <leonardosaeta1@gmail.com>
2021-03-26 00:56:52 +0000

    Update design
    Co-Authored-By: WajaTheHat <73642192+WajaTheHat@users.noreply.github.com>
    Co-Authored-By: Mirna <73242980+mirnaalsaeed@users.noreply.github.com>

--- front/src/Components/LandingPage/LandingIndex.jsx
+++ front/src/Components/LandingPage/LandingIndex.jsx
@@ -15,45 +15,42 @@ class LandingIndex extends Component {
                 <div className="singUp">
                     <div>
                         <label className='acc' for="acc">Accomplish more with Noteware</label>
-                        <Link to="/Signup" style={{ textDecoration: 'none' }}className="UP">SIGN UP HERE </Link  >
+                        <Link to="/Signup" style={{ textDecoration: 'none' }} className="UP">SIGN UP HERE </Link  >
+                    </div>
+                </div>
+                <div className="bodyD">
+                    <div className="firstDiv">
+                        <div className="aboutBox">
+                            <p>WHAT IS NOTEWARE?</p>
+                            <div className="aboutBox1">
+                                <p>NOTEWARE is a notetaking app that aims to address the UN's goal to provide Quality Education for everyone.
+                                <br />
+                                    <br />
+                                </p>
+                                <br />
+                            </div>
+                        </div>
+
+                        <div className="secondDiv">
+                            <img className="collaboration" src={notes} alt="" />
+                            <div className="secondBox">
+                                <div className="benfits"> <b></b>
+                                    <p> BENIFITS OF USING NOTEWARE </p>
+                                    <br></br>
+                                    <br></br>
+                                    <p> Store notes- Best place to keep all of your notes safe </p>
+                                    <br></br>
+                                    <p> Notesfeed- You can share your notes with your friends!</p>
+                                    <br></br>
+                                    <p> Comment section- You can receive feedbacks from your colleagues and help them by commenting your thoughts!</p>
+                                    <br></br>
+                                    <p> Votes and Ranks- You can vote what the best notes and resume!</p>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
-                <main>
-                    <div className="firstBox">
-                        <div className="box">
-                            <img className="collaboration" src={notes} alt="" />
-
-                        </div>
-
-                        <div className="aboutBox" >
-                            <p className="what">WHAT IS NOTEWARE?</p>
-                            <div className="aboutBox1">
-                                <p>
-                                    NOTEWARE is a notetaking app that aims to address the UN's goal to provide Quality Education for everyone.
-                                <br></br>
-                                    <br></br>
-                                </p>
-                                <br></br>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="secondBox">
-                        <div className="benfits"> <b></b>
-                            <p> BENIFITS OF USING NOTEWARE </p>
-                            <br></br>
-                            <br></br>
-                            <p> Store notes- Best place to keep all of your notes safe </p>
-                            <br></br>
-                            <p> Notesfeed- You can share your notes with your friends!</p>
-                            <br></br>
-                            <p> Comment section- You can receive feedbacks from your colleagues and help them by commenting your thoughts!</p>
-                            <br></br>
-                            <p> Votes and Ranks- You can vote what the best notes and resume!</p>
-                        </div>
-                    </div>
-                </main>
-                <Footer />
             </section>
         );
     }
