7743933a5823959f370e14b51b20876b07692e74
Marta Ignatowcz <msmartaignatowicz@gmail.com>
2021-08-08 15:19:47 +0100

    Setting up new server

--- Back-End/noteware/src/main/java/com/group15/noteware/NotewareApplication.java
+++ Back-End/noteware/src/main/java/com/group15/noteware/NotewareApplication.java
@@ -2,6 +2,7 @@ package com.group15.noteware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class NotewareApplication {
