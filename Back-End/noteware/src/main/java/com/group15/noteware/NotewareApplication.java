d0ffbbd8e23e50ed3a82a9b24292976140feaa7c
Marta Ignatowcz <msmartaignatowicz@gmail.com>
2021-08-09 13:50:15 +0100

    Minor changes to the code

--- Back-End/noteware/src/main/java/com/group15/noteware/NotewareApplication.java
+++ Back-End/noteware/src/main/java/com/group15/noteware/NotewareApplication.java
@@ -2,9 +2,10 @@ package com.group15.noteware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.group15.noteware"})
 public class NotewareApplication {
 
 	public static void main(String[] args) {
