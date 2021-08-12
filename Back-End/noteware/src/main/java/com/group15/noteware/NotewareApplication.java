1f47f53712d6f790b3093684e661e7aa51a48da0
Marta Ignatowcz <msmartaignatowicz@gmail.com>
2021-08-12 17:54:32 +0100

    Deleted unnecessary line

--- Back-End/noteware/src/main/java/com/group15/noteware/NotewareApplication.java
+++ Back-End/noteware/src/main/java/com/group15/noteware/NotewareApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
-@ComponentScan(basePackages = "com.baeldung.componentscan") // this line has no reason to exist but the program does not give generated security password without it
 public class NotewareApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(NotewareApplication.class, args);
 	}
 
