44e90895f2d8854e1632dd6720cdd040d3f2ed73
Marta Ignatowcz <msmartaignatowicz@gmail.com>
2021-08-12 21:41:04 +0100

    Deleted DeleteUserRequest

--- Back-End/noteware/src/main/java/com/group15/noteware/controllers/DeleteUserController.java
+++ Back-End/noteware/src/main/java/com/group15/noteware/controllers/DeleteUserController.java
@@ -1,13 +1,10 @@
 package com.group15.noteware.controllers;
 
-import com.group15.noteware.models.Category;
 import com.group15.noteware.models.User;
 import com.group15.noteware.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import static com.group15.noteware.payload.request.DeleteUserRequest.getId;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -18,8 +15,7 @@ public class DeleteUserController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(User id) {
-        userRepository.delete(id);
-    }
+    public void deleteUser(User id) { userRepository.delete(id); }
+
 
 }
